7c47dd5ff5f75eaf1f0aa2338ddf981efa97d7d8
Mario Fernandez <mario.fernandez@hceris.com>
2012-12-15 17:14:00 +0100

    Add some more packages

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,5 +1,5 @@
 node basenode {
-  $basics = ['curl', 'vim', 'git', 'ack-grep', 'wget']
+  $basics = ['curl', 'vim', 'git', 'ack-grep', 'wget', 'htop', 'tmux', 'zip']
   package { $basics: ensure => "latest" }
 
   stage { 'first': before => Stage['main'] }
